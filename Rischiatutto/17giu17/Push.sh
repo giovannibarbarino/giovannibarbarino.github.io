346251ffd32db2bdd7cba576a7e24a029a46f250
exodd <giovanni.barbarino@gmail.com>
2023-07-14 13:57:16 +0200

    Add files via upload

---
+++ Rischiatutto/17giu17/Push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+                   
+                   LOC_DIR_P='/home/exodd/Dropbox/università/Rischiatutto/html/Rischiatutto/17giu17/index.html'
+                   REM_DIR_P=''
+                   SERVER='poisson.phc.dm.unipi.it'
+                   USER="barbarino"
+                   PS="ganedacobi"
+                   DATES_LIST_FILE='dates_list.txt'
+                   
+                   sshpass -p "$PS" scp $LOC_DIR_P $USER@$SERVER:~/public_html/Rischiatutto/17giu17/
